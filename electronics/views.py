8704cde2100db131dbaa143703d89aa52d3f7d48
Pavantamhane <pavantamhane1164@gmail.com>
2025-03-19 16:54:03 +0530

    second

--- electronics/views.py
+++ electronics/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import product
+from .forms import productForm
 # Create your views here.
 def home(request):
     return render(request,'electronics/home.html')
 
 def product_detail(request):
     prod=product.objects.all()
-    return render(request,'electronics/product_detail.html',{"prod":prod})
+    form = productForm()
+    if request.method == 'POST':
+        form = productForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'electronics/product_detail.html',{"prod":prod,"form":form})
